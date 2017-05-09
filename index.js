46c6483b98e11a8137ce526b6cd5e34de10693dd
mic pantakan <pantakan@paysbuy.com>
2017-05-09 16:45:52 +0700

    implemented bot post

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ var bot = new Twit({
     access_token_secret: process.env.MIG_BOT_ACCESS_TOKEN_SECRET,
     timeout_ms: 60*1000
 });
+
+bot.post('statuses/update' , { status :'hello world from twit bot !!' }
+    , function (err, data, response){
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(data.text + ' was tweeted');
+        }
+    }
+)
